8c5ca43fd1ff0bf3169ee668668e27061f3c7edb
Jayjader <njay47@gmail.com>
2017-02-23 22:11:30 +0100

    fix

--- main.py
+++ main.py
@@ -39,7 +39,6 @@ class EndPoint(object):
             for cache in self.caches:
                 if request.video in cache.videos:
                     score += calculate_score(self.latency, self.caches[cache.id])
-
         return score
 
 class Video(object):
@@ -56,7 +55,9 @@ class Request(object):
 
 
 if __name__ == '__main__':
-    input_set = "me_at_the_zoo.in"
+    if len(sys.argv) != 2:
+        sys.exit(-1)
+    input_set = sys.argv[1]
     input_file = open(input_set).readlines()
 
     line = input_file.pop(0)
@@ -70,18 +71,20 @@ if __name__ == '__main__':
     endpoints = [None for i in range(nbEndPoints)]  # type: List(Endpoint)
     caches = [None for i in range(nbCache)]  # type: List(Cache)
     requests = [None for i in range(nbRequest)]  # type: List(Request)
-    print(caches)
+    #print(caches)
 
     line = input_file.pop(0)
     video_sizes = line.split(' ')
+    print("videos...")
     for i in range(nbVideos):
         v = Video(id=i, size=int(video_sizes[i]))
         videos[i] = v
-    print("videos:", videos)
+    #print("videos:", videos)
 
     # Endpoint i
+    print("endpoints...")
     for i in range(nbEndPoints):
-        print("endpoint", i)
+        #print("endpoint", i)
         line = input_file.pop(0)
         latency, nb_caches_endpoint = (int(i) for i in line.split(' '))
         if not endpoints[i]:
@@ -89,21 +92,22 @@ if __name__ == '__main__':
 
         # Cache j
         for j in range(nb_caches_endpoint):
-            print("cache", j, "in endpoint", i)
+            #print("cache", j, "in endpoint", i)
             line = input_file.pop(0)
             cache_id, latency = (int(i) for i in line.split(' '))
-            print("cache id:", cache_id)
+            #print("cache id:", cache_id)
             if not caches[cache_id]:
                 caches[cache_id] = Cache(id=cache_id, capaMax=capaCache, endPoints=[])
                 caches[cache_id].capaDispo = capaCache
             endpoints[i].caches[cache_id] = latency
 
     # Request i
+    print("requests")
     for i in range(nbRequest):
         line = input_file.pop(0)
         id_video, id_endpoint, number_requests = [int(i) for i in line.split(' ')]
         request = Request(times=number_requests, video=videos[id_video], endpoint=endpoints[id_endpoint])
-        print("request", i, ":", "endpoint", id_endpoint)
+        #print("request", i, ":", "endpoint", id_endpoint)
         endpoints[id_endpoint].requests.append(request)
 
 
@@ -120,20 +124,20 @@ if __name__ == '__main__':
             sorted_caches.append((cache_id, endpoint.caches[cache_id]))
         sorted_caches.sort(key=lambda couple: couple[1])
         if endpoint.requests != []:
-            print("endpoint has requests")
+            #print("endpoint has requests")
             request = endpoint.requests[0]
             for cache_id in endpoint.caches.keys():
                 cache = caches[cache_id]
-                print("cache", cache_id)
+                #print("cache", cache_id)
                 if request.video.id not in cache.videos:
-                    print("video not yet in cache")
+                    #print("video not yet in cache")
                     if cache.capaDispo > request.video.size:
                         cache.videos.append(request.video.id)
                         cache.capaDispo -= request.video.size
                         if not cache.used:
                             cache.used = True
                             caches_used.append(cache.id)
-                            print("cache", cache.id, "is storing video", request.video.id)
+                            #print("cache", cache.id, "is storing video", request.video.id)
                         break
         #endpoint.requests.sort(key=lambda r: r.times*endpoint.latency/r.video.size)
         #endpoint.caches.sort(key=lambda c: c[1])
