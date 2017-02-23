53d0e3c2318dd5d0e21fbbe6f22a33fe25432a01
Lucien Haurat <lucien@haur.at>
2017-02-23 19:16:49 +0100

    début parseur

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ G = nx.Graph()
 G.add_edge(1,2)
 nx.draw(G)
 plt.show()
+
+if __name__ == '__main__':
+    input_set = "me_at_the_zoo.in"
+    input_file = open(input_set).readlines()
+
+    line = input_file.pop(0)
+    nbVideos, nbEndPoints, nbRequest, nbCache, capaCache = line.split(' ')
