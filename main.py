1950a50c2a5232a2bf4a560452b2c4869f8f77b1
Jayjader <njay47@gmail.com>
2017-02-23 19:19:33 +0100

    scores

--- main.py
+++ main.py
@@ -5,3 +5,20 @@ G = nx.Graph()
 G.add_edge(1,2)
 nx.draw(G)
 plt.show()
+
+
+
+
+
+def calculate_score(lat_data, lat_cache):
+    return lat_data - lat_cache
+
+def endpoint_score(endpoint):
+    score = 0
+
+    for request in endpoint.requests:
+        for cache in endpoint.caches:
+            if request.video in cache.videos:
+                score += calculate_score(endpoint.latency, endpoint.caches[cache.id])
+
+    return score
